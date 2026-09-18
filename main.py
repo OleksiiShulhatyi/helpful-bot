62c459278f024f3eae50c251148249e7f213db65
OleksiiShulhatyi <117361549+OleksiiShulhatyi@users.noreply.github.com>
2026-09-18 15:51:05 +0200

    A new file has been added

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+Додаток, який буде зберігати додатки
+
+This is my note, that I am taking on my laptop
+- Created on 19.12.2024 20:15 ❤️
+
+[("This is my note, that I am taking on my laptop", "19.12.2024 20:15")]
+[("19.12.2024 20:15", "This is my note, that I am taking on my laptop", )]
+
+{"text": "This is my note, that I am taking on my laptop", "creation_date": "19.12.2024 20:15"}
+
+1) Написати цикл який буде отримувати інформацію від користувача та реагувати на неї
+
+'''
